f9691245f0f65f560af5bb87770a130993d8b7b7
Sarah Graywood <smgraywood@gmail.com>
2023-02-20 16:21:22 -0800

    adding background color changing js file

--- random_background.js
+++ random_background.js
@@ -1,23 +1,26 @@
-    const colors = 
-        [
-        "EA6123",
-        "CB4603",
-        "AB2A01",
-        "8C0300",
-        "6D0200",
-        "4F0100",
-        "350002",
-        "010000",
-        "000000",
-        "ea0c0c",
-        "940194",
-        "3fab43"
-        ];
+$(document).ready(function () {
+	const colors = [
+		"EA6123",
+		"CB4603",
+		"AB2A01",
+		"8C0300",
+		"6D0200",
+		"4F0100",
+		"350002",
+		"010000",
+		"000000",
+		"ea0c0c",
+		"940194",
+		"3fab43",
+	];
 
-    const randomColor = () => {
-		return '#' + Math.floor(Math.random() * colors.length);
-	};
+	const randomColor = () => {
+        let random = Math.floor(Math.random() * colors.length);
+		let colorRandom = "#" + colors[random];
+        return colorRandom;
+    };
 
-	$('#button').on('click', function() {
-		$('body').css('background', randomColor());
+	$("button").on("click", function () {
+		$("body").css("background-color", randomColor());
 	});
+});
